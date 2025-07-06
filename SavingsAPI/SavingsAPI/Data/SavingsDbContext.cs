afae80b7de52bad43bd6f656939b23aaad4b67a6
Leonixu <107299451+Leonixu@users.noreply.github.com>
2025-07-06 14:13:52 +1000

    Add Backend BankAcc Model and Db Context

---
+++ SavingsAPI/SavingsAPI/Data/SavingsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SavingsAPI.Models;
+
+namespace SavingsAPI.Data
+{
+    public class SavingsDbContext : DbContext
+    {
+        public SavingsDbContext(DbContextOptions<SavingsDbContext> options)
+            : base(options) { }
+
+        public DbSet<SavingsAcc> SavingsAccounts { get; set; }
+    }
+}
---
+++ SavingsAPI/SavingsAPI/Models/SavingsAcc.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SavingsAPI.Models
+{
+    public class SavingsAcc
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        required public string Name { get; set; }
+        [Required]
+        required public string Bank { get; set; }
+        [Required]
+        public required string BankProductId { get; set; }
+        public string? BonusConditions { get; set; }
+        [Required]
+        required public float BaseRate { get; set; }
+        public float BonusRate { get; set; }
+        [Required]
+        public float TotalRate { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public required string URL { get; set; }
+
+    }
+}
