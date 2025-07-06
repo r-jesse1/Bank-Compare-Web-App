ce012215501e7e6259677ce5cf905ea63b1547d3
Leonixu <107299451+Leonixu@users.noreply.github.com>
2025-07-06 14:13:06 +1000

    Add Backend Product Model

---
+++ SavingsAPI/SavingsAPI/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SavingsAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? ProductId { get; set; }
+        public string? ProductCategory { get; set; }
+        public string? Name { get; set; }
+        public string? BrandName { get; set; }
+    }
+
+    public class ProductData
+    {
+        public List<Product> Products { get; set; }
+    }
+
+    public class Root
+    {
+        public ProductData Data { get; set; }
+    }
+}
