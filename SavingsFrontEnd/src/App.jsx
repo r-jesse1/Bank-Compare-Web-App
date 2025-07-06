578cd30eafcc2f9733d87bb3433bb9c4ef2f1e05
Leonixu <107299451+Leonixu@users.noreply.github.com>
2025-07-06 16:07:07 +1000

    Add react Router and basic savings cards

--- SavingsFrontEnd/src/App.jsx
+++ SavingsFrontEnd/src/App.jsx
@@ -1,30 +1,18 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 import "@mantine/core/styles.css";
-
+import { Savings } from "./Savings";
 import { MantineProvider } from "@mantine/core";
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <MantineProvider>
-      <>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Savings />} />
+        </Routes>
+      </Router>
     </MantineProvider>
   );
 }
